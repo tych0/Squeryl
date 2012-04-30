f0bb4f885cec17ee7020febe8e9321db2bd9b4ef
Tycho Andersen <tycho.andersen@wicourts.gov>
2012-04-30 10:40:37 -0500

    build with 2.9.1

--- project/SquerylBuild.scala
+++ project/SquerylBuild.scala
@@ -24,7 +24,7 @@ object SquerylBuild extends Build {
   			  },
     		  parallelExecution := false,
     		  publishMavenStyle := true,
-  			  scalaVersion := "2.9.2",
+  			  scalaVersion := "2.9.1",
   			  crossScalaVersions := Seq("2.9.2", "2.9.1", "2.9.0-1", "2.9.0", "2.8.1", "2.8.0"),
   			  licenses := Seq("Apache 2" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt")),
   			  homepage := Some(url("http://squeryl.org")),
