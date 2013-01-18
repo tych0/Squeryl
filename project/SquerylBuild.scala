c6c3a002f4613db6f8b482985812859e91fa9f2d
Peter Brant <peter.brant@gmail.com>
2013-01-18 21:28:22 +0100

    Cross-build only the versions we care about

--- project/SquerylBuild.scala
+++ project/SquerylBuild.scala
@@ -34,8 +34,9 @@ object SquerylBuild extends Build {
 	  },
       parallelExecution := false,
       publishMavenStyle := true,
-      scalaVersion := "2.10.0",
-      crossScalaVersions := Seq("2.10.0", "2.9.2", "2.9.1", "2.9.0-1", "2.9.0"),
+      scalaVersion := "2.9.2",
+      // crossScalaVersions := Seq("2.10.0", "2.9.2", "2.9.1", "2.9.0-1", "2.9.0"),
+      crossScalaVersions := Seq("2.9.2", "2.9.1"),
       licenses := Seq("Apache 2" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt")),
       homepage := Some(url("http://squeryl.org")),
       pomExtra := (<scm>
